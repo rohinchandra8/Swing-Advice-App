456b9941b06d54ebadf692f92632207659dac6f7
Rohin Chandra <rohinchandra@gmail.com>
2026-04-22 21:19:21 -0400

    Revert "fix image routing through proxy"
    This reverts commit 32ace435c777a172fa0427f526d229a7110d40ea.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/ai-swing-coach',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
